d12c6d7623846c8253f740e1a751f8a59d450812
scherepanov <sergey@cherepanov.org.ua>
2023-02-05 19:32:08 +0000

    update docker compose

--- start-with-slave.sh
+++ start-with-slave.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ./start.sh
-docker-compose -p openvpn-slave -f docker-compose-slave.yaml up -d
+docker compose -p openvpn-slave -f docker-compose-slave.yaml up -d
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 export OVPN_PUBLIC_IP=$(host myip.opendns.com resolver1.opendns.com | grep 'has address' | awk '{ print $4}')
 
-docker-compose -p openvpn-master up -d --build
+docker compose -p openvpn-master up -d --build
