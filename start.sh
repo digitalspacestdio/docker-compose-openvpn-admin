3f2f94880ebbde45d857596c20bdd0fcb10add20
scherepanov <sergey@cherepanov.org.ua>
2023-02-05 19:39:33 +0000

    v4

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export OVPN_PUBLIC_IP=$(host myip.opendns.com resolver1.opendns.com | grep 'has address' | awk '{ print $4}')
+export OVPN_PUBLIC_IP=$(host -4 myip.opendns.com resolver1.opendns.com | grep 'has address' | awk '{ print $4}')
 
 docker compose -p openvpn-master up -d --build
