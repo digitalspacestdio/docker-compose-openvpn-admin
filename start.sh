2365a9bef5479239fed03eb3719b49d932a00ae7
root <root@v94169200.local>
2023-02-05 19:23:43 +0000

    tuning

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+export OVPN_PUBLIC_IP=$(host myip.opendns.com resolver1.opendns.com | grep 'has address' | awk '{ print $4}')
+
 docker-compose -p openvpn-master up -d --build
